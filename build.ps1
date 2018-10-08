3b853daade9a6e52f1f647d236e988ba7e881884
Isabella Muerte <63051+slurps-mad-rips@users.noreply.github.com>
2018-10-07 22:16:15 -0700

    :boom: Add Richard Smith's email to implicit module partitions
    :bug: Fix typo in build script
    :memo: Update more wording for desert sessions paper. Unfortunately too
    much is left to chance for its interface so we'll be holding back on
    wording for San Diego.

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
 New-Item -ItemType Directory -Force -Path html
-Get-ChildItem .\proposals\ | %{
+Get-ChildItem ./proposals/ | %{
   curl https://api.csswg.org/bikeshed/ -F file='@'$_ -F force=1 > ("html/{0}.html" -f $_.BaseName)
 }
